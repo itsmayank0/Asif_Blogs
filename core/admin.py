5e10ef006ce66d24eacffc88073d925c3f6ce904
Mayank dhakad <mayankirar@gmail.com>
2020-06-12 12:00:00 -0700

    Author added

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BlogPost, EmailList
+from .models import BlogPost, Author, HomeBanner
 
 # Register your models here.
 
@@ -7,17 +7,20 @@ admin.site.site_header = "Asif Blog Admin View"
 
 
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date_of_publish', 'published', 'author_name')
+    list_display = ('title', 'date_of_publish',
+                    'published', 'featured', 'author')
     list_filter = ['date_of_publish', 'published']
-    search_fields = ['title', 'author_name']
+    search_fields = ['title']
 
 
-class EmailListAdmin(admin.ModelAdmin):
-    list_display = ('user_name', 'user_email')
-    list_filter = ['user_name']
-    search_fields = ['user_name', 'user_email']
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+class HomeBannerAdmin(admin.ModelAdmin):
+    list_filter = ['date', 'featured']
 
 
 admin.site.register(BlogPost, BlogPostAdmin)
-
-admin.site.register(EmailList, EmailListAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(HomeBanner, HomeBannerAdmin)
