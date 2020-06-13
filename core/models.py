e38e2d03d43cf8b101944e485eb0fe88e04d8ca5
Mayank dhakad <mayankirar@gmail.com>
2020-06-13 16:12:02 +0530

    some important Classes

--- core/models.py
+++ core/models.py
@@ -2,12 +2,23 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.encoding import smart_text
+from django.contrib.auth import get_user_model
 
 
 PUBLISH_CHOICES = [
     ('private', 'Private'),
     ('public', 'Public'),
 ]
+
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='pics')
+
+    def __str__(self):
+        return self.user.username
 
 
 class BlogPost(models.Model):
@@ -16,12 +27,14 @@ class BlogPost(models.Model):
             "unique": "Enter unique title."
         })
     post_description = models.TextField(blank=True, null=True)
-    post_short_description = models.TextField(default="Default short des.")
-    post_images = models.ImageField(upload_to='pics', blank=True)
-    date_of_publish = models.DateField(default=timezone.now)
+    overview = models.TextField(default="Default short des.")
+    thumbnail = models.ImageField(upload_to='pics', blank=True)
+    date_of_publish = models.DateField(auto_now_add=True)
     published = models.CharField(
         max_length=250, choices=PUBLISH_CHOICES, default=None)
-    author_name = models.CharField(max_length=250)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    featured = models.BooleanField(default=False)
+    youtube = models.BooleanField(default=False)
     slug = models.SlugField(max_length=1000, blank=True)
 
     def save(self, *args, **kwargs):
@@ -34,10 +47,12 @@ class BlogPost(models.Model):
         verbose_name_plural = 'Blog Posts'
 
 
-class EmailList(models.Model):
-    user_name = models.CharField(max_length=300)
-    user_email = models.EmailField()
+class HomeBanner(models.Model):
+    banner_image = models.ImageField(upload_to='pics')
+    banner_Heading = models.CharField(max_length=1000)
+    banner_description = models.TextField()
+    date = models.DateField(default=timezone.now)
+    featured = models.BooleanField(unique=True, blank=True, null=True)
 
-    class Meta:
-        verbose_name = 'Users Email List'
-        verbose_name_plural = 'Users Email List'
+    def __str__(self):
+        return self.banner_Heading
